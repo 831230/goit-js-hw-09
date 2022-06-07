55a242e272601f7359d0fea3b4966347443be5f7
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-06-07 14:12:11 +0200

    Zadanie nr 9
    część 1 ukończone

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBg = document.querySelector('body');
+let timerID = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener("click", function () {
+  timerID = setInterval(() => {
+    bodyBg.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+  startBtn.setAttribute('disabled', true)
+});
+
+stopBtn.addEventListener("click", function () {
+  clearInterval(timerID);
+  startBtn.removeAttribute('disabled')
+});
