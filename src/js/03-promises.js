01025495ea20a2a0bc4ea7a2468f5651a4760121
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-06-13 15:08:11 +0200

    Zadanie nr 9
    część trzecia ukończona

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,27 +5,6 @@ const delayStepInput = document.querySelector('input[name="step"]');
 const amountInput = document.querySelector('input[name="amount"]');
 const submitBtn = document.querySelector('button[type="submit"]');
 
-// let timer = 0;
-
-// function createPromise(position, timer) {
-//   return new Promise((resolve, reject) => { 
-//     setTimeout(() => { 
-//       const shouldResolve = Math.random() > 0.3;
-//     if (shouldResolve) {
-//         // Fulfill
-//       resolve({ position, timer });
-//       } else {
-//         // Reject
-//       reject({ position, timer });
-//       }
-//     });
-//   }, timer);
-// };
-
-
-
-
-// ===========================================================================================
 let timer = 0;
 
 function createPromise(position, timer) {
@@ -68,43 +47,3 @@ submitBtn.addEventListener("click", function (event) {
       timer += delayStep;
     };
 });
-// ==================================================================================================
-
-
-// submitBtn.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   firstDelay = firstDelayInput.value; 
-//   delayStep = delayStepInput.value; 
-//   amount = amountInput.value;
-//   for (let i = 0; i < amount; i++) { 
-//     setTimeout(() => { 
-
-//       function createPromise(i, delayStep) {
-//         return new Promise((resolve, reject) => { 
-//           const shouldResolve = Math.random() > 0.3;
-//           if (shouldResolve) {
-//             // Fulfill
-//             resolve()   
-//           } else {
-//             // Reject
-//             reject()
-//           }
-//         })
-
-//       };
-      
-//       createPromise(i, delayStep)
-//         .then(({ i, delayStep }) => {
-//           console.log(`✅ Fulfilled promise ${i} in ${delayStep}ms`);
-//         })
-//         .catch(({ i, delayStep }) => {
-//           console.log(`❌ Rejected promise ${i} in ${delayStep}ms`);
-//         });
-
-//     },firstDelay);
-//   };
-
-// });
-
-
-
